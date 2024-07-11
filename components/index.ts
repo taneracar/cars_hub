fe7dede6f59a80cd844c1f5ae248076ce4d83107
Taner Acar <151092957+taneracar@users.noreply.github.com>
2024-07-11 10:44:15 +0300

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+import CarDetails from "./CarDetails";
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    ShowMore,
+    CarDetails,
+}
